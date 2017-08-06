2c8b75a54d11b0f58c536d8116b1549f492ab79a
Brooke Babineau <ba.babineau@gmail.com>
2017-08-06 11:49:34 -0700

    fix #1 add red line

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -6,6 +6,7 @@ ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
   geom_point()
 
 ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + 
-  geom_line() +
-  geom_point(aes(color = continent))
+  geom_point(aes(color = continent)) +
+  scale_x_log10() +
+  geom_smooth(method="lm", color = "red")
   
